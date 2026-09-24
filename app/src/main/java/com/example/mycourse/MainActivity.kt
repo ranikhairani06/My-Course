06f8a45905f8dc180dbea753b70a0f743862a3d7
ranikhairani06 <nikmahrani006@gmail.com>
2026-09-24 09:24:42 +0700

    update app

--- app/src/main/java/com/example/mycourse/MainActivity.kt
+++ app/src/main/java/com/example/mycourse/MainActivity.kt
@@ -6,10 +6,19 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2,
+            R.string.tab_text_3
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +27,22 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(
-                systemBars.left,
-                systemBars.top,
-                systemBars.right,
-                systemBars.bottom
-            )
-            insets
-        }
+        // inisialisasi adapter
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+
+        // inisialisasi view pager
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+
+        viewPager.adapter = sectionsPagerAdapter
+
+        // inisialisasi tab layout
+        val tabs: TabLayout = findViewById(R.id.tab_layout)
+
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+        supportActionBar?.elevation = 0f
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -44,7 +59,6 @@ class MainActivity : AppCompatActivity() {
             }
 
             R.id.action_materi -> {
-                // Logika untuk melihat skor
                 Toast.makeText(this, "Materi", Toast.LENGTH_SHORT).show()
                 true
             }
