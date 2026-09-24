6353c9916e1ed3b2e809bf6ab9ebd122b8a7725a
ranikhairani06 <nikmahrani006@gmail.com>
2026-09-24 08:43:55 +0700

    initial commit

---
+++ app/src/main/java/com/example/mycourse/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.mycourse
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+            insets
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+
+            R.id.action_home -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.action_materi -> {
+                // Logika untuk melihat skor
+                Toast.makeText(this, "Materi", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.action_quiz -> {
+                Toast.makeText(this, "Quiz", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
